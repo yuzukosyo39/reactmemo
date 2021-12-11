f23fc8945ddc9cea75da2c9d77692ae08e13987d
markn <k7.markn@gmail.com>
2021-12-11 13:51:16 +0000

    REACTMEMO-14 作成に失敗したため、GraphQL APIとデータベースを削除

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "reactmemo": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
