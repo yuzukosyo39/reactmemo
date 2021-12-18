aac479fa0841a0e52b0c524149d34eb265852986
markn <k7.markn@gmail.com>
2021-12-18 23:17:47 +0900

    REACTMEMO-23 ソース管理からバックエンドのAmplifyを除外

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "reactmemo": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
